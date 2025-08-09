7597d20e6f607e744f75e2eadad0b7d6dd3bc478
bdenesh123 <drajan@wvb.com>
2025-08-09 23:02:44 +0800

    complete page section updates

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,11 @@ const Loader = () => {
   return (
     <Html>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+        <div className="flex space-x-2">
+          <span className="w-2 h-2 bg-[#afafaf] rounded-full animate-bounce [animation-delay:0ms]"></span>
+          <span className="w-2 h-2 bg-[#afafaf] rounded-full animate-bounce [animation-delay:200ms]"></span>
+          <span className="w-2 h-2 bg-[#afafaf] rounded-full animate-bounce [animation-delay:400ms]"></span>
+        </div>
       </div>
     </Html>
   );
